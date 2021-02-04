7ec1f9d162d17931557a90c191b9140273167c44
Filmon-coder <eaglefifi@gmail.com>
2021-02-03 21:58:07 -0500

    Part1 - Balance display finished

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express= require ('express')
 const app= express ()
-const port=(3000)
+const port= 3000
+const bal= "25000"
+const dep= "15000"
 
 app.get('/',(req, res) =>{
  res.send('Hello World!')
 
+})
+app.get('/balance', (req, res) => {
+    res.send(`balance: $${bal}`)
 
-});
+})
+
+app.get('/deposit', (req, res) => {
+    res.send(`Deposit: $${dep}`)
+
+})
+
 app.listen(port,()=>{
  console.log ('Bitcoin Server')
 
